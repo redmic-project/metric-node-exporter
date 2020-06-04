48d56e771ca40524bb144d9314d3ba679a0d112b
Pedro Trujillo <pedroetb@gmail.com>
2020-06-04 11:11:26 +0100

    Configura deploy de servicio, usa imagen oficial

---
+++ deploy/config/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+nodeName=$(cat "${CONFIG_PATH}/nodename")
+
+echo "node_meta{\
+node_id=\"${NODE_ID}\",\
+container_label_com_docker_swarm_node_id=\"${NODE_ID}\",\
+node_name=\"${nodeName}\"\
+} 1" > "${CONFIG_PATH}/node-meta.prom"
+
+set -- /bin/node_exporter "${@}"
+
+exec "${@}"
